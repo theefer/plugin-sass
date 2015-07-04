90aa2a11053f9fdf7ee5f02d448301602348e8cb
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-07-04 15:46:35 +0100

    Use sass.js exclusively even to build, as node-sass is broken via jspm

--- sass-builder.js
+++ sass-builder.js
@@ -1,18 +1,41 @@
+// TODO: or use more efficient node-sass (issues with installing it via jspm)
+var Sass = require('sass.js/dist/sass.sync');
 
-var sass = require('node-sass');
+// FIXME: avoid copying this boilerplate from the plugin-css...
+function escape(source) {
+	return source
+		.replace(/(["\\])/g, '\\$1')
+		.replace(/[\f]/g, "\\f")
+		.replace(/[\b]/g, "\\b")
+		.replace(/[\n]/g, "\\n")
+		.replace(/[\t]/g, "\\t")
+		.replace(/[\r]/g, "\\r")
+		.replace(/[\u2028]/g, "\\u2028")
+		.replace(/[\u2029]/g, "\\u2029");
+}
+
+var cssInject = "(function(c){var d=document,a='appendChild',i='styleSheet',s=d.createElement('style');s.type='text/css';d.getElementsByTagName('head')[0][a](s);s[i]?s[i].cssText=c:s[a](d.createTextNode(c));})";
+
+function compileSass(source) {
+  return new Promise(function(resolve, reject) {
+    Sass.compile(source, function(result) {
+      // FIXME: source maps etc?
+      resolve(result.text);
+    });
+    // FIXME: reject if errors?
+  });
+
+}
 
 module.exports = function(loads, opts) {
-  return new Promise(function(resolve, reject) {
-    sass.render({
-      data: loads.source
-      // [, options..]
-    }, function(err, result) {
-      console.log("COMPILED", result);
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
+  var stubDefines = loads.map(function(load) {
+    return "System\.register('" + load.name + "', [], false, function() {});";
+  }).join('\n');
+
+  return Promise.all(loads.map(function(load) {
+    return compileSass(load.source);
+  })).then(function(compiledCss) {
+    var cssOutput = compiledCss.join('\n');
+    return [stubDefines, cssInject, '("' + escape(cssOutput) + '");'].join('\n');
   });
-}
+};
