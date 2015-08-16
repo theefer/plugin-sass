33b67017ffe730bb8130c8b6a79c1b167bcece90
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-08-16 16:32:45 +0100

    More work on sass-builder

--- sass-builder.js
+++ sass-builder.js
@@ -1,6 +1,29 @@
 // TODO: or use more efficient node-sass (issues with installing it via jspm)
 var Sass = require('sass.js/dist/sass.sync');
 
+// Turn on source maps
+// Sass.options({
+//     // Embed included contents in maps
+//     sourceMapContents: true,
+//     // Embed sourceMappingUrl as data uri
+//     sourceMapEmbed: true,
+//     // Disable sourceMappingUrl in css output
+//     sourceMapOmitUrl: false
+// });
+
+// Resolve @imports
+var fs = require('fs');
+Sass.importer(function(request, done) {
+    var url = request.current;
+    fs.readFile(url, function(err, text) {
+        if (err) {
+            done({path: url, error: err.message});
+        } else {
+            done({path: url, content: text});
+        }
+    });
+});
+
 // FIXME: avoid copying this boilerplate from the plugin-css...
 function escape(source) {
 	return source
@@ -16,13 +39,20 @@ function escape(source) {
 
 var cssInject = "(function(c){var d=document,a='appendChild',i='styleSheet',s=d.createElement('style');s.type='text/css';d.getElementsByTagName('head')[0][a](s);s[i]?s[i].cssText=c:s[a](d.createTextNode(c));})";
 
-function compileSass(source) {
+function compileSass(source, url) {
   return new Promise(function(resolve, reject) {
-    Sass.compile(source, function(result) {
-      // FIXME: source maps etc?
-      resolve(result.text);
+    // FIXME: resolve imports
+    // FIXME: source maps etc?
+    Sass.compile(source, {
+      inputPath: url.replace('file://', '')
+    }, function(result) {
+      var successful = result.status === 0;
+      if (successful) {
+        resolve(result.text);
+      } else {
+        reject(result.formatted);
+      }
     });
-    // FIXME: reject if errors?
   });
 
 }
@@ -33,7 +63,7 @@ module.exports = function(loads, opts) {
   }).join('\n');
 
   return Promise.all(loads.map(function(load) {
-    return compileSass(load.source);
+    return compileSass(load.source, load.address);
   })).then(function(compiledCss) {
     var cssOutput = compiledCss.join('\n');
     return [stubDefines, cssInject, '("' + escape(cssOutput) + '");'].join('\n');
